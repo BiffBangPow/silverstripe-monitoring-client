983aece4765c0f294bc8559739697d5b70da7d1e
Tim Burt <tim@biffbangpow.com>
2023-04-11 16:41:50 +0100

    Update controller functionality Remove test class

--- src/Control/ClientController.php
+++ src/Control/ClientController.php
@@ -2,6 +2,11 @@
 
 namespace BiffBangPow\SSMonitor\Client\Control;
 
+use BiffBangPow\SSMonitor\Client\Helper\EncryptionHelper;
+use BiffBangPow\SSMonitor\Client\Module\AllPackageVersions;
+use BiffBangPow\SSMonitor\Client\Module\CorePackageVersions;
+use BiffBangPow\SSMonitor\Client\Module\SSConfiguration;
+use BiffBangPow\SSMonitor\Client\Module\SystemInfo;
 use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Environment;
@@ -11,9 +16,33 @@ class ClientController extends ContentController
     public function index(HTTPRequest $request)
     {
         $api_key = Environment::getEnv('MONITORING_API_KEY');
-        $headerKey = $request->postVar('key');
-        if ((!$api_key) || ($api_key !== $headerKey)) {
+        $authKey = $request->postVar('key');
+        if ((!$api_key) || ($api_key !== $authKey)) {
             return $this->httpError(404);
         }
+
+        return $this->getReportResponse();
+    }
+
+
+    private function getReportResponse()
+    {
+        $classes = [
+            CorePackageVersions::class,
+            SystemInfo::class,
+            SSConfiguration::class,
+            AllPackageVersions::class
+        ];
+
+        $result = [];
+
+        foreach ($classes as $class) {
+            $classOut = (new $class)->getResult();
+            $result = array_merge($result, $classOut);
+        }
+
+        $resultJson = serialize($result);
+        $encHelper = new EncryptionHelper();
+        return $encHelper->encrypt($resultJson);
     }
 }
