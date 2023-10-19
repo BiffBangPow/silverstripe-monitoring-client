6407b002c530155583eb522b6b223ed1db97bed5
Tim Burt <tim@biffbangpow.com>
2023-10-19 14:43:21 +0100

    Update build tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,25 @@
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
-const { parallel, watch, src, dest } = require('gulp');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const mode = require('gulp-mode')({
-    modes: ["production", "development"],
-    default: "development",
-    verbose: false
+
+const sourceDir = './client/src/scss';
+const destinationDir = './client/dist/css';
+
+
+gulp.task('sass', function () {
+    return gulp
+        .src(`${sourceDir}/*.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(destinationDir));
 });
 
-function processcss() {
-    return src('client/src/scss/**/*.scss')
-        .pipe(mode.development(sourcemaps.init()))
-        .pipe(sass({
-            includePaths: ['node_modules']
-        })).on('error', sass.logError)
-        .pipe(autoprefixer({'grid': 'no-autoplace'}))
-        .pipe(mode.production(cleanCSS({level:1, inline: ['local']})))
-        .pipe(mode.development(sourcemaps.write('../../dist/css/maps')))
-        .pipe(dest('client/dist/css'))
-}
 
-function transpileJS() {
-    return src('client/src/javascript/**/*.js')
-        .pipe(mode.development(sourcemaps.init()))
-        .pipe(babel({
-            presets: [
-                '@babel/preset-env'
-            ]
-        }))
-        .pipe(mode.production(uglify()))
-        .pipe(mode.development(sourcemaps.write('../../dist/javascript/maps')))
-        .pipe(dest('client/dist/javascript'))
-}
+gulp.task('watch', function () {
+    gulp.watch(`${sourceDir}/*.scss`, gulp.series('sass'));
+});
 
 
-function watchfiles() {
-    watch('client/src/javascript/**/*.js', {queue: false}, transpileJS);
-    watch('client/src/scss/**/*.scss', {queue: false}, processcss);
-}
-
-exports.default = parallel(transpileJS, processcss);
-exports.watch = watchfiles;
+gulp.task('default', gulp.series('sass', 'watch'));
